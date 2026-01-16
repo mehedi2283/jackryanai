84606f380006ff234d76041a45c9c30c276de8ec
mehedi2283 <mehedihasan123456789.mh.mh@gmail.com>
2026-01-17 03:20:42 +0600

    first commit34

--- index.tsx
+++ index.tsx
@@ -8,6 +8,8 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
+console.log(import.meta.env.VITE_GEMINI_API_KEY);
+
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
